6701864a5ededb9c53d20767f39659d3e55f3240
Adam Hellberg <sharparam@sharparam.com>
2021-04-28 00:54:16 +0200

    Document root module

--- lib/purtea.rb
+++ lib/purtea.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+# A tool to handle importing of FF Logs TEA stats to a Google Sheet.
+module Purtea; end
+
 require 'purtea/version'
 require 'purtea/config'
+require 'purtea/logging'
 
 require 'purtea/fflogs'
 require 'purtea/sheet'
